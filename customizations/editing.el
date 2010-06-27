0c2dbb5444ea93b167ffce4b6637b1fb3b2572a4
Anton I. Sipos <aisipos@gmail.com>
2010-06-26 19:41:48 -0700

    Some editing settings borrowed from Alex Payne.

---
+++ customizations/editing.el
@@ -0,0 +1,44 @@
+; tabs
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+; encoding
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+; whitespace
+;(global-whitespace-mode t)
+(setq show-trailing-whitespace t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;selection
+(pc-selection-mode)
+
+; show column number in bar
+(column-number-mode t)
+
+; highlight URLs in comments/strings
+(add-hook 'find-file-hooks 'goto-address-prog-mode)
+
+; selection
+(delete-selection-mode t)
+
+; show marks as selections
+(setq transient-mark-mode t)
+
+; highlight matching parens
+(show-paren-mode t)
+
+; highlight incremental search
+(defconst search-highlight t)
+
+; no newlines past EOF
+(setq next-line-add-newlines nil)
+
+
+; smooth scrolling
+;;(require 'centered-cursor-mode)
+;;(global-centered-cursor-mode t)
