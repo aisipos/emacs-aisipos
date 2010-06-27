5fa7a345e62bdf955cc7f10afb4e278bf19aadc1
Anton I. Sipos <aisipos@gmail.com>
2010-06-26 19:44:59 -0700

    First attempt to start maximized on Linux

--- linux-customizations/ui.el
+++ linux-customizations/ui.el
@@ -1,3 +1,12 @@
 ; nice font
 ;;(set-default-font "-unknown-DejaVu Sans Mono-medium-normal-normal-*-20-*-*-*-m-0-iso10646-1")
 (push '(font . "Liberation Mono-10") default-frame-alist)
+
+(defun toggle-fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+(toggle-fullscreen)
