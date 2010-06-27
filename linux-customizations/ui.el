08d03f18447a8eea8786255f477fe8ec1dda7aaf
Anton I. Sipos <aisipos@gmail.com>
2010-06-26 19:57:31 -0700

    Added maximization on win32 at startup

--- linux-customizations/ui.el
+++ linux-customizations/ui.el
@@ -10,3 +10,5 @@
 	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
 )
 (toggle-fullscreen)
+(toggle-fullscreen)
+
---
+++ win-customizations/ui.el
@@ -0,0 +1,2 @@
+;; Maximize - from http://stackoverflow.com/questions/815239/how-to-maximize-emacs-on-windows-at-startup
+(w32-send-sys-command 61488)
