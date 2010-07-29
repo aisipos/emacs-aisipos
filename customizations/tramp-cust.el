e2628d9bdd9d004d015f6bad7ac87d921b043e87
Anton I. Sipos <aisipos@gmail.com>
2010-07-28 20:00:11 -0700

    Ensure tramp is loaded before customizing it

--- customizations/tramp-cust.el
+++ customizations/tramp-cust.el
@@ -1,6 +1,7 @@
 ;;Allow //sudo:hostname
+(require 'tramp)
 ;;See http://www.gnu.org/software/tramp/#Multi_002dhops
 (add-to-list 'tramp-default-proxies-alist
-             '(nil "\\`root\\'" "/ssh:%h:"))
+           '(nil "\\`root\\'" "/ssh:%h:"))
 (add-to-list 'tramp-default-proxies-alist
              '((regexp-quote (system-name)) nil nil))
