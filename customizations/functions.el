90982cd36416b1c65f91c23759dd583898bba0e7
Anton I. Sipos <aisipos@gmail.com>
2011-01-05 16:50:57 -0800

    Add a pretty print xml function that works in nxml mode

--- customizations/functions.el
+++ customizations/functions.el
@@ -1,6 +1,23 @@
 ;;Custom functions
 
 ;;Seems to be needed for search-at-point
+;;Found at http://www.emacswiki.org/emacs/CompilationMode
 (defun delete-this-overlay(overlay is-after begin end &optional len)
   (delete-overlay overlay)
 )
+
+;;See http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
